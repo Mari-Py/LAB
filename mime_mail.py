3babb65da9949701e62d7f3500254ac52db0cb77
Mari-Py <79842111+Mari-Py@users.noreply.github.com>
2022-08-08 14:51:22 +0300

    Update mime_mail.py

--- mime_mail.py
+++ mime_mail.py
@@ -4,12 +4,12 @@ from email.message import EmailMessage
 msg = EmailMessage()
 msg.set_content("The body of the email")
 msg["Subject"] = "Test1"
-msg["From"] = "test01235@bk.ru"
-msg["To"] = "beretta07@bk.ru"
+msg["From"] = "_"
+msg["To"] = "_"
 
 context=ssl.create_default_context()
 
 with smtplib.SMTP("smtp.mail.ru", port=587) as smtp:
     smtp.starttls(context=context)
-    smtp.login(msg["From"], "WiIY2OcQSjQ6komBrLNs")
+    smtp.login(msg["From"], "_")
     smtp.send_message(msg)
